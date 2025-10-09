4b2c18fe61da6eed7b6bc86ff638a4c2444c0ba0
ritwik mandal <ritwikmandal2015@gmail.com>
2025-10-09 17:18:54 +0530

    first project

---
+++ first.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>My Story</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, nulla!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+Error impedit maxime quos delectus velit hic eveniet suscipit quisquam,
+quae repellendus illum blanditiis ab aspernatur accusamus nisi quod, 
+fuga iusto perferendis odit soluta. 
+Quae perspiciatis ullam aut ut dolor ipsum fuga quis repellat.
+Soluta alias nobis eum nemo modi, debitis similique!</p>
+<?php
+
+echo "Ritwik Mandal";
+$name="ritwik";
+echo "<br>";
+
+echo "my name is " . $name;
+$number=76;
+echo "<br>";
+echo "my name is " .$name. " And My roll number is ".$number ;
+
+echo "<br>";
+$array=array("html","css","python","java");
+print_r($array);
+
+echo "<br>";
+
+$boolean=false;
+echo ($boolean? 'true': 'false');
+
+echo "<br>";
+
+$float=5.5;
+echo "the float value is ". $float . "<br>";
+
+$num =30;
+$num1=40;
+$sum=$num+$num1;
+echo "the sum of two number is ". $sum;
+
+
+
+?>
+
+
+</body>
+</html>
