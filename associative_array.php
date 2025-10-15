6dc05ce33e322b4176b43c42a36dd811c5346b9d
ritwik <ritwikmandal2015@gmail.com>
2025-10-15 06:41:34 +0530

    new file

--- associative_array.php
+++ associative_array.php
@@ -5,4 +5,11 @@ echo "user name is ".$user["name"]. "</br>";
 echo "user name is ".$user["age"]. "</br>";
 echo "user name is ".$user["gender"]. "</br>";
 
+echo "<hr>";
+
+$student=["name"=>"ritwik","age"=>"25", "gender"=>"male"];
+echo "user name is ".$student["name"]. "</br>";
+echo "user name is ".$student["age"]. "</br>";
+echo "user name is ".$student["gender"]. "</br>";
+
 ?>
---
+++ indexArray.php
@@ -0,0 +1,21 @@
+<?php
+
+$user=array("ritwik","ram","nilu","raj");
+echo "name is " .$user[2]. "</br>";
+echo "name is " .$user[1]. "</br>";
+echo "name is " .$user[0]. "</br>";
+echo "name is " .$user[3]. "</br>";
+echo "<hr>";
+
+
+
+$student=["ritwik","ram","nilu","raj"];
+echo "name is " .$user[2]. "</br>";
+echo "name is " .$user[1]. "</br>";
+echo "name is " .$user[0]. "</br>";
+echo "name is " .$user[3]. "</br>";
+
+
+
+
+?>
