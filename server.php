b8582f515cd722f34e2d8cb4cbfcda0b37beea1a
ritwik <ritwikmandal2015@gmail.com>
2025-10-24 18:07:59 +0530

    add server

---
+++ server.php
@@ -0,0 +1,12 @@
+
+<?php
+
+echo "This file is: " . $_SERVER['PHP_SELF'] . "<br>";
+echo "Server name: " . $_SERVER['SERVER_NAME'] . "<br>";
+echo "Request method: " . $_SERVER['REQUEST_METHOD'] . "<br>";
+echo "User agent: " . $_SERVER['HTTP_USER_AGENT'] . "<br>";
+echo "Script name: " . $_SERVER['SCRIPT_NAME'] . "<br>";
+echo "date time: " . date("Y-m-d H:i:s") . "<br>";
+
+
+?>
